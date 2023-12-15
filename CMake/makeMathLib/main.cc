52ea78cf560bbb84724efc5eaa295d8b375b1de0
Junhui Li <junhui.lee@icloud.com>
2023-12-15 16:25:28 +0800

    add code

---
+++ CMake/makeMathLib/main.cc
@@ -0,0 +1,9 @@
+#include "mymath.h"
+#include <iostream>
+int main()
+{
+    std::cout << "Hello world" << std::endl;
+    std::cout << add(3, 5) << std::endl;
+    return 0;
+}
+
---
+++ CMake/makeMathLib/mathFunc/mymath.h
@@ -0,0 +1,5 @@
+#ifndef MY_MATH_H_
+#define MY_MATH_H_
+
+int add(int a, int b);
+#endif
