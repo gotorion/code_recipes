b657784194e519bcd063d3b4c4be6b81d9fa8269
Junhui Li <junhui.lee@icloud.com>
2024-05-11 17:44:22 +0800

    add code

---
+++ CMake/demo/src/add.cpp
@@ -0,0 +1,3 @@
+template <typename T> decltype(auto) add(T left, T right) {
+  return left + right;
+}
--- cplusplus/cpp_11/initializer_list.cc
+++ cplusplus/cpp_11/initializer_list.cc
@@ -2,30 +2,54 @@
 #include <vector>
 using WordList = std::vector<std::string>;
 
-class LexicalRepo
-{
-public:
-  explicit LexicalRepo(const std::initializer_list<const char *> &words)
-  {
+class LexicalRepo {
+ public:
+  explicit LexicalRepo(const std::initializer_list<const char*>& words) {
     wordList_.insert(std::begin(wordList_), std::begin(words), std::end(words));
   }
 
-private:
+ private:
   WordList wordList_;
 };
 
-void test_lexical_repo() { LexicalRepo repo{"Hello", "world"}; }
+void test_lexical_repo() {
+  LexicalRepo repo{"Hello", "world"};
+}
 
-struct Pig
-{
+struct Pig {
   // 这里是没有写构造函数的
   std::string name_;
   int age_;
 };
 
-int main()
-{
-  Pig redPig{"Petty", 18}; // C++11自动生成列表初始化函数
-  test_lexical_repo();
+class Person {
+ public:
+  Person(std::initializer_list<std::string> val) noexcept {
+    // note that initializer_list usuaual not use for this condition
+    auto iter = std::begin(val);
+    name_ = *iter;
+    iter++;
+    country_ = *iter;
+    iter++;
+    address_ = *iter;
+  }
+  void print() const {
+    std::cout << "Name : " << name_ << std::endl;
+    std::cout << "Country : " << country_ << std::endl;
+    std::cout << "Address : " << address_ << std::endl;
+  }
+
+ private:
+  std::string name_;
+  std::string country_;
+  std::string address_;
+};
+
+int main() {
+  // Pig redPig{"Petty", 18}; // C++11自动生成列表初始化函数
+  // test_lexical_repo();
+  //
+  Person me{"John", "US", "SaltLake City"};
+  me.print();
   return 0;
 }
