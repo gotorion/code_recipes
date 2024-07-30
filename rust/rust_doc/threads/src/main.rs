cfe62ffbea8cd94776488c06706eab173b7b5e02
Junhui Li <junhui.lee@icloud.com>
2024-07-30 22:49:54 +0800

    add code

---
+++ rust/rust_doc/threads/src/main.rs
@@ -0,0 +1,27 @@
+use std::thread;
+use std::time::Duration;
+fn use_handle() {
+    let handle = thread::spawn(|| {
+        for i in 1..10 {
+            println!("hi number {i} from the spawned thread!");
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+    for i in 1..5 {
+        println!("hi number {i} from the main thread!");
+        thread::sleep(Duration::from_millis(1));
+    }
+    handle.join().unwrap();
+}
+
+fn move_thread() {
+    let v = vec![1, 2, 3];
+    let handle = thread::spawn(move || {
+        println!("Here's a vector: {:?}", v);
+    });
+    handle.join().unwrap();
+}
+fn main() {
+    use_handle();
+    move_thread();
+}
