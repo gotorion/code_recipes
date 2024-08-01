204bfeb2e5ebd2f622eba760ffe7bcc5d7090911
Junhui Li <junhui.lee@icloud.com>
2024-08-01 22:39:17 +0800

    add rust mutex

--- rust/rust_doc/threads/src/main.rs
+++ rust/rust_doc/threads/src/main.rs
@@ -1,4 +1,5 @@
 use std::sync::mpsc;
+use std::sync::{Arc, Mutex};
 use std::thread;
 use std::time::Duration;
 fn use_handle() {
@@ -41,8 +42,28 @@ fn use_channel() {
         println!("Got: {receive}");
     }
 }
+
+fn multi_thread() {
+    let counter = Arc::new(Mutex::new(0));
+    let mut handles = vec![];
+
+    for _ in 0..10 {
+        let counter = Arc::clone(&counter);
+        let handle = thread::spawn(move || {
+            let mut num = counter.lock().unwrap();
+            *num += 1;
+        });
+        handles.push(handle);
+    }
+    for handle in handles {
+        handle.join().unwrap();
+    }
+    println!("Result: {}", *counter.lock().unwrap());
+}
+
 fn main() {
     use_handle();
     move_thread();
     use_channel();
+    multi_thread();
 }
